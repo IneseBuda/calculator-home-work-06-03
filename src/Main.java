12c218a148bfd466c30ae677dab042fd9f3ea5c0
Ineseb <inesebuda@inbox.lv>
2023-03-07 19:05:19 +0200

    Corection to double

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,30 @@ public class Main {
         System.out.println("meal costs calculator");
         System.out.println("Calculation summ of two meals");
         System.out.println("Provide lunch costs in eur: ");
-        int lunchCosts;
-        int dinnerCosts;
+        double lunchCosts;
+        double dinnerCosts;
         int numberofPersons;
         int costsonePerson;
-        int calculationPeriod;
-        //TODO: explain object creation
+        double calculationPeriod;
+
         Scanner inputReader = new Scanner(System.in);
-        lunchCosts = inputReader.nextInt();
+        lunchCosts = inputReader.nextDouble();
         System.out.println("You provided costs in eur:" + lunchCosts);
         System.out.println("Provide dinner costs in eur: ");
-        dinnerCosts = inputReader.nextInt();
+        dinnerCosts = inputReader.nextDouble();
         System.out.println("You provided cocts in eur:" + dinnerCosts);
-        int sum = lunchCosts + dinnerCosts;
+        double sum = lunchCosts + dinnerCosts;
         System.out.println("Sum of meal costs in eur is: " + sum);
         System.out.println("Provide number of persons in meal:");
         numberofPersons = inputReader.nextInt();
         System.out.println("You provided number of persons in meals:" + numberofPersons);
-        int division = ((lunchCosts + dinnerCosts) / numberofPersons);
+        double division = ((lunchCosts + dinnerCosts) / numberofPersons);
         System.out.println("Costs for one person in eur is:" + division);
-        System.out.println("Provide calculation period in days:");
-        calculationPeriod = inputReader.nextInt();
-        System.out.println("You provided calculation period in days: " + calculationPeriod);
-        int multiplication = division * calculationPeriod;
-        System.out.println("Costs for calculation period in eur (max 9 numbers):" + multiplication);
+        System.out.println("Provide calculation period in days (max 9 numbers):");
+        calculationPeriod = inputReader.nextDouble();
+        System.out.println("You provided calculation period in days : " + calculationPeriod);
+        double multiplication = division * calculationPeriod;
+        System.out.println("Costs for calculation period in eur :" + multiplication);
 
 
 
