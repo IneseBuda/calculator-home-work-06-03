81fb8181c16075e130dfdebd6ecc7d4a6f0bde5e
Ineseb <inesebuda@inbox.lv>
2023-03-07 17:04:50 +0200

    adding lunch and dinner costs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("meal costs calculator");
         System.out.println("Calculation difference of two numbers");
-        System.out.println("Provide lunch costs number: ");
+        System.out.println("Provide lunch costs in eur: ");
+        int lunchCosts;
+        int dinnerCosts;
+        //TODO: explain object creation
+        Scanner inputReader = new Scanner(System.in);
+        lunchCosts = inputReader.nextInt();
+        System.out.println("You provided costs in eur:" + lunchCosts);
+        System.out.println("Provide dinner costs in eur: ");
+        dinnerCosts = inputReader.nextInt();
+        System.out.println("You provided cocts in eur:" + dinnerCosts);
+
 
 
     }
