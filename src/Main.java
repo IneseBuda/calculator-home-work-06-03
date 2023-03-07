eab52d57e02451af09b5c1c6d6d98a204df026a8
Ineseb <inesebuda@inbox.lv>
2023-03-07 17:53:34 +0200

    adding multiplicatin for calculation period

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         int dinnerCosts;
         int numberofPersons;
         int costsonePerson;
+        int calculationPeriod;
         //TODO: explain object creation
         Scanner inputReader = new Scanner(System.in);
         lunchCosts = inputReader.nextInt();
@@ -17,12 +18,25 @@ public class Main {
         dinnerCosts = inputReader.nextInt();
         System.out.println("You provided cocts in eur:" + dinnerCosts);
         int sum = lunchCosts + dinnerCosts;
-        System.out.println("Sum of meal costs is: " + sum);
+        System.out.println("Sum of meal costs in eur is: " + sum);
         System.out.println("Provide number of persons in meal:");
         numberofPersons = inputReader.nextInt();
         System.out.println("You provided number of persons in meals:" + numberofPersons);
         int division = ((lunchCosts + dinnerCosts) / numberofPersons);
         System.out.println("Costs for one person in eur is:" + division);
+        System.out.println("Provide calculation period in days:");
+        calculationPeriod = inputReader.nextInt();
+        System.out.println("You provided calculation period in days: " + calculationPeriod);
+        int multiplication = division * calculationPeriod;
+        System.out.println("Costs for calculation period in eur:" + multiplication);
+
+
+
+
+
+
+
+
 
 
 
